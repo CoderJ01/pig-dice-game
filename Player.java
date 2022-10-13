effef8d897bf580138127434aef9c76cf672ad7b
Joshua <buildingguy@sbcglobal.net>
2022-10-13 12:40:55 -0700

    create Player getters

--- Player.java
+++ Player.java
@@ -9,5 +9,22 @@ public class Player {
     public Player(String playerName) {
         this.name = playerName;
     }
+
+    // getters
+    public String getName() {
+        return this.name;
+    }
+
+    public int getFirstDieNumber() {
+        return this.dice1;
+    }
+
+    public int getSecondDieNumber() {
+        return this.dice2;
+    }
+
+    public int getPoints() {
+        return this.points;
+    }
     
 }
