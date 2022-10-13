76960ca5250f1347e3c3f77eada8dc939be256f5
Joshua <buildingguy@sbcglobal.net>
2022-10-13 13:49:48 -0700

    test dice roll

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Player joshua = new Player("Joshua");
+        int a = joshua.rollFirstDice();
+        int b = joshua.rollSecondDice();
+        System.out.println(a + " " + b);
     }
 }
