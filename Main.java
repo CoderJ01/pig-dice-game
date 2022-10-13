f204c87845eb8ceff1ef7d1b6864703cb768aea4
Joshua <buildingguy@sbcglobal.net>
2022-10-13 14:51:36 -0700

    end the game when one of the players gets 100 points

--- Main.java
+++ Main.java
@@ -5,25 +5,54 @@ public class Main {
         CPU CPU1 = new CPU("CPU_1");
         CPU CPU2 = new CPU("CPU_2");
         CPU CPU3 = new CPU("CPU_3");
-        
-        // playable character
-        int a = joshua.rollFirstDice();
-        int b = joshua.rollSecondDice();
-        joshua.determinePoints(a, b);
 
-        // CPU 1
-        int c = CPU1.rollFirstDice();
-        int d = CPU1.rollSecondDice();
-        CPU1.determinePoints(c, d);
+        // initilize parameters with points
+        int w = joshua.getPoints();
+        int x = CPU1.getPoints();
+        int y = CPU2.getPoints();
+        int z = CPU3.getPoints();
 
-        // CPU 2
-        int e = CPU2.rollFirstDice();
-        int f = CPU2.rollSecondDice();
-        CPU2.determinePoints(e, f);
+        // continue the game while all the players have less than 100 points
+        while(w < 100 && x < 100 && y < 100 && z < 100) {
+            // playable character
+            int a = joshua.rollFirstDice();
+            int b = joshua.rollSecondDice();
+            joshua.determinePoints(a, b);
 
-        // CPU 3
-        int g = CPU3.rollFirstDice();
-        int h = CPU3.rollSecondDice();
-        CPU1.determinePoints(g, h);
+            if(w >= 100) {
+                System.out.println(joshua.getName() + " wins the game with " + joshua.getPoints() + " points");
+                break;
+            }
+
+            // CPU 1
+            int c = CPU1.rollFirstDice();
+            int d = CPU1.rollSecondDice();
+            CPU1.determinePoints(c, d);
+
+            if(x >= 100) {
+                System.out.println(CPU1.getName() + " wins the game with " + CPU1.getPoints() + " points");
+                break;
+            }
+
+            // CPU 2
+            int e = CPU2.rollFirstDice();
+            int f = CPU2.rollSecondDice();
+            CPU2.determinePoints(e, f);
+
+            if(y >= 100) {
+                System.out.println(CPU2.getName() + " wins the game with " + CPU2.getPoints() + " points");
+                break;
+            }
+
+            // CPU 3
+            int g = CPU3.rollFirstDice();
+            int h = CPU3.rollSecondDice();
+            CPU1.determinePoints(g, h);
+
+            if(z >= 100) {
+                System.out.println(CPU3.getName() + " wins the game with " + CPU3.getPoints() + " points");
+                break;
+            }
+        }
     }
 }
