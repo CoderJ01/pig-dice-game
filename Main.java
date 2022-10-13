5c14e2d6831439650efd7206b8b6c37b9131cb88
Joshua <buildingguy@sbcglobal.net>
2022-10-13 14:35:15 -0700

    clean

--- Main.java
+++ Main.java
@@ -1,17 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        // Player joshua = new Player("Joshua");
-        // int a = joshua.rollFirstDice();
-        // int b = joshua.rollSecondDice();
-        // System.out.println(a + " " + b);
+        // players
+        Player joshua = new Player("Joshua");
+        CPU CPU1 = new CPU("CPU_1");
+        CPU CPU2 = new CPU("CPU_2");
+        CPU CPU3 = new CPU("CPU_3");
+        
+        // playable character
+        int a = joshua.rollFirstDice();
+        int b = joshua.rollSecondDice();
+        joshua.determinePoints(a, b);
 
-        // joshua.determinePoints(a, b);
-
-        CPU CPU1 = new CPU("CPU1");
+        // CPU 1
         int c = CPU1.rollFirstDice();
         int d = CPU1.rollSecondDice();
-        System.out.println(c + " " + d);
-
         CPU1.determinePoints(c, d);
+
+        // CPU 2
+        int e = CPU2.rollFirstDice();
+        int f = CPU2.rollSecondDice();
+        CPU2.determinePoints(e, f);
+
+        // CPU 3
+        int g = CPU3.rollFirstDice();
+        int h = CPU3.rollSecondDice();
+        CPU1.determinePoints(g, h);
     }
 }
